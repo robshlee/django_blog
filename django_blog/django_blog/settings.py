3f9e6c01e6b9eb0b03289ead880196b07bc2b230
Rob Lee <robshlee@generalassemb.ly>
2020-08-21 21:45:29 -0400

    edit: push working copy of s3

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -35,8 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'media.apps.MediaConfig',
     'django.contrib.staticfiles',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -117,11 +117,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_URL = '/staticfiles/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL = '/media/' # how we're going to access media in browser
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # directory where uploaded files are saved
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
@@ -140,14 +136,31 @@ AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
 
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION = 'static'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'blog/static/blog'),
+]
+
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/' # how we're going to access media in browser
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media') # directory where uploaded files are saved
+
 AWS_S3_FILE_OVERWRITE = False #makes sure files aren't being overwritten when uploaded
 AWS_DEFAULT_ACL = None
 
-# DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 #check to see if debug mode
 try:
-    if os.environ['DEBUG'] == 'true':
+    if os.environ['DEBUG'] == 1:
         DEBUG = 1
     else:
         DEBUG = 0
@@ -183,3 +196,4 @@ if DEBUG:
         'INTERCEPT_REDIRECTS': False,
         'SHOW_TEMPLATE_CONTEXT': True,
     }
+
